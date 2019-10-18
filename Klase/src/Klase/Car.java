3b165fd83ec35eaa82b3ee33cbee926b7d2e8cef
okkursevi8 <okkursevi8@UC5PC4>
2019-10-18 21:31:00 +0200

    Klasa pocetak

---
+++ Klase/src/Klase/Car.java
@@ -0,0 +1,14 @@
+package Klase;
+
+public class Car {
+	
+	public String boja;
+	public String marka;
+	public String model;
+	public int cena;
+	public double kubikaza;
+	public boolean daljinskoZakljucavanje;
+	
+	
+	
+}
---
+++ Klase/src/Klase/GlavnaKlasa.java
@@ -0,0 +1,32 @@
+package Klase;
+
+public class GlavnaKlasa {
+
+	public static void main(String[] args) {
+		
+		Car automobil = new Car(); //instanca klase Car
+		
+		automobil.boja = "crvena";
+		automobil.cena = 150000;
+		automobil.kubikaza = 4.8;
+		automobil.marka = "ferari";
+		automobil.model = "La ferrara";
+		automobil.daljinskoZakljucavanje = true;
+		
+		System.out.println("Napravim sam auto marke " + automobil.marka);
+		System.out.println("Model je: " + automobil.model);
+		System.out.println("Boje: " + automobil.boja);
+		System.out.println("Koji kosta: " + automobil.cena);
+		System.out.println("I koji ima " + automobil.kubikaza + " L ");
+		System.out.println("Daljinsko zakljucavanje: " + automobil.daljinskoZakljucavanje);
+		
+		Car automobil2 = new Car();
+		
+		automobil2.marka = "Mc Laren";
+		automobil2.model = "Sena";
+		
+		System.out.println("Najbrzi automobil je " + automobil2.marka + " " + automobil2.model);
+		
+	}
+
+}
